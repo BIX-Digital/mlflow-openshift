00c5fe6f800cc929e6e94e9a08f9ab6f8f2046ac
sklingel <stefan.klingelschmitt@gmail.com>
2021-01-04 13:37:37 +0100

    refactor remaining items for openshift client

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mlflow_openshift",
-    version="0.0.3",
+    version="0.0.6",
     author="sklingel",
     description="MLFlow Openshift Deployment Package",
     long_description=long_description,
