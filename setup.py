b81865b56827d203d7233b5b9fc31a48747c340e
sklingel <stefan.klingelschmitt@gmail.com>
2021-01-07 14:52:22 +0100

    updated version in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mlflow_openshift",
-    version="0.1.0",
+    version="0.1.1",
     author="sklingel",
     description="MLFlow Openshift Deployment Plugin",
     long_description=long_description,
